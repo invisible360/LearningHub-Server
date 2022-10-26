4ed880fd43b51fc23f059a07679072f2ec3824d4
Zakir Hossain <zakirh817@gmail.com>
2022-10-26 12:00:23 +0600

    fake data created

--- index.js
+++ index.js
@@ -2,18 +2,23 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-const courses = require ('./data/Courses.json')
+const courses = require('./data/Courses.json');
+const course = require('./data/Course.json');
 
 const app = express();
 
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send(`<h1 style="text-align: center">IT HUB Server is Running...</h1>`);
+    res.send(`<h1 style="text-align: center">Programing HUB Server is Running...</h1>`);
 })
 
-app.get ('/courses', (req, res)=> {
-    res.send (courses);
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+
+app.get('/course', (req, res) => {
+    res.send(course);
 })
 
 app.listen(port, () => {
